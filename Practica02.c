899c9ce11021c79cbe82f0ad777e31aa83f01248
kevinkrg <36976835+kevinkrg@users.noreply.github.com>
2018-03-08 18:42:39 -0800

    Update Practica02.c

--- Practica02.c
+++ Practica02.c
@@ -1,72 +1,50 @@
+
+#include <sys/types.h>
+#include <unistd.h>
 #include <stdio.h>
 
+main(){
+	int return_value;
+	int numero;
 
-int main()
+	printf("\n Introduzca un numero : \n");
+	scanf("%d", &numero);
 
-{
+	printf("Creando procesos con fork\n");
+	return_value=fork();
+	if(return_value==0){
+		printf("El pid del proceso hijo es %d\n", getpid());
 	
-int return_value;
-	
-int numero;
-	
-
-	
-printf("\n Introduzca un numero : \n");
-	
-    
-scanf("%d", &numero);
-
-
-	
-	
-
-printf("Creando procesos con fork\n");
-
-	
-	
-printf("\n Creando proceso hijo \n");{
-	
-	            
-int factorial = 1;
+		int factorial = 1;
     
 
-for(int i = 1; i <= numero; i++){
+		for(int i = 1; i <= numero; i++){
        
 
- factorial = factorial * i;
-    }
+ 		factorial = factorial * i;
+ }
     
    
  
-printf("El factorial de %d es %d \n", numero , factorial);
-	
-	
-}
-	
-		
+		printf("El factorial de %d es %d \n", numero , factorial);
 
-printf("\nCreando proceso padre \n");{
-		            
+	}
+	else{
+		printf("El pid del proceso padre es %d\n", getpid());
 
-int sumatoria = 0;
-        for(int i = 0; i <= numero; i++){
+
+			int sumatoria = 0;
+        	for(int i = 0; i <= numero; i++){
      
    
-sumatoria = sumatoria + i;
+			sumatoria = sumatoria + i;
         
 
 }
     
-    
-
-printf("La sumatoria de %d es %d \n", numero, sumatoria);
-	
-	
-}
-        
-
-
+ 
+			printf("La sumatoria de %d es %d \n", numero, sumatoria);
 }
 
-
+}
 
